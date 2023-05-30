6b3bc9ebd53b4c6f896f55b472582a262bffcd40
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-05-30 22:40:56 +0200

    update

--- app/api/authorize/route.ts
+++ app/api/authorize/route.ts
@@ -4,7 +4,7 @@ const clientSecret = process.env.CLIENT_SECRET
 const clientId = process.env.CLIENT_ID
 const redirectURI = 'https://infojobsito.vercel.app/login'
 
-const BASE_URL_API = 'https://www.infojobs.net'
+const BASE_URL_API = 'http://www.infojobs.net/core/oauth2vc/index.xhtml'
 
 export async function GET(request: Request) {
   const code = request.headers.get('InfoJobs-Code')
